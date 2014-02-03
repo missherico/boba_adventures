389d527b5fab4e48253747bc5b7b3516fcf66f12
Isa Herico <miss.herico@gmail.com>
2014-02-02 19:49:51 -0800

    first commit, added yelp gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WheresMyBoba::Application.config.secret_key_base = 'f7a53691ff28f10797124dee791ee07025e4c7fac7fd3a3fe35f4c41821330a7d46358c82290f657e78cf8a3f2a720a182b485e195f41e35587c7111e70ecc0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WheresMyBoba::Application.config.session_store :cookie_store, key: '_wheres_my_boba_session'
