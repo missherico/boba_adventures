345ddad75cab6ca0deed772217223947acc24824
Isa Herico <miss.herico@gmail.com>
2014-02-04 17:49:06 -0800

    starting adventures to user relationship

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -12,18 +12,33 @@ include AdventuresHelper
 
   def create
   	new_adventure = params.require(:adventure).permit(:name, :description, :city, :state, :address, :cross_st1, :cross_st2)
+    new_adventure[:user_id]= current_user.user_id
+    @adventure = Adventure.create(new_adventure)
+
+    render :show
   end
 
   def show
+  	@adventure = Adventure.find(params[:id])
   end
 
   def edit
+  	@adventure = Adventure.find(params[:id])
   end
 
   def update
+    @adventure = current_user.adventures.where(:id => params[:id])
+    @adventure.update_attributes(params[:adventure])
+
+    render :show
   end
 
   def destroy
+  	adventure = current_user.adventures.where(:id => params[:id])
+  	adventure.delete
+  	redirect_to(adventures_path)
+
+
   end
 
 end
