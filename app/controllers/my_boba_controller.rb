bff9f9b0f0992534528d84abb7cbc76f0f9e0b13
Isa Herico <miss.herico@gmail.com>
2014-02-07 01:14:21 -0800

    added map and marker, recalled api to render active locations

--- app/controllers/my_boba_controller.rb
+++ app/controllers/my_boba_controller.rb
@@ -8,30 +8,21 @@ class MyBobaController < ApplicationController
   def results
     location = params[:location]
     @user_loc = (location[0]).to_s.upcase
-
     results = Bobalocation.get_results(@user_loc)
-    
     @boba_locations = results['businesses']   
     @neighborhood_array = (results['businesses']).sort_by { |biz| biz["location"]['neighborhood']}
     @rated_array = (results['businesses']).sort_by { |biz| biz['rating']}
-
     geocoder = Graticule.service(:google).new ENV['GOOGLE_API_KEY']
     loc = geocoder.locate("#{location}")
-
     lat = loc.latitude
     long = loc.longitude
-
-
-
     render :results
   end
 
   def show
     @id = params[:yelp_id]
     @user = current_user
-
     @result = Bobalocation.show_location(@id)
-
     @rating = @result['rating_img_url']
     yelp_id = @result['id']
     hood = @result['location']['neighborhoods']
@@ -42,14 +33,23 @@ class MyBobaController < ApplicationController
     addy.insert(1, @result['location']['city'])
     addy.insert(2, @result['location']['state_code'])
     location = addy.flatten 
-    
     geocoder = Graticule.service(:google).new ENV['GOOGLE_API_KEY']
     loc = geocoder.locate("#{location}")
-
     lat = loc.latitude
     long = loc.longitude
-    boba = Bobalocation.create(yelp_id: yelp_id, latitude: lat, longitude: long)
-    gon.locations = boba
+    @boba = Bobalocation.create(yelp_id: yelp_id, latitude: lat, longitude: long)
+    gon.locations = @boba
+
+    a = @result['location']['address']
+    b = a.join.to_s
+    # coords[0] = [@boba.latitude, @boba.longitude]
+    yelp = load_yelp
+
+    @adventures = yelp.search_by_category_filter_and_location('active', b)
+    cats = @adventures['businesses'].map { |cat| cat['categories']}
+
+binding.pry
+
   end
 
 
--- app/models/bobalocation.rb
+++ app/models/bobalocation.rb
@@ -42,8 +42,6 @@ class Bobalocation < ActiveRecord::Base
 
 
 
-
-
 private
 
   def self.load_yelp
